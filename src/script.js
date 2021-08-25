c89ef1449e77ad9713d8579a0414212e68306627
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-25 06:58:54 +0100

    Add background to dropdown depending on content size

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
 const triggers = document.querySelectorAll(".cool > li");
 const nav = document.querySelector(".top");
-const background = document.querySelector("dropdownBackground");
+const background = document.querySelector(".dropdownBackground");
 
 function handleEnter() {
   this.classList.add("trigger-enter");
   setTimeout(() => this.classList.add("trigger-enter-active"), 150);
+  background.classList.add("open");
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+  };
+  background.style.setProperty("height", `${coords.height}px`);
+  background.style.setProperty("width", `${coords.width}px`);
 }
 function handleLeave() {
   this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
 }
 
 triggers.forEach((trigger) =>
