e824bdd66d31ca605691d9588111dd164b89a05f
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-25 07:12:44 +0100

    Manage setTimeout when mouseEnter

--- src/script.js
+++ src/script.js
@@ -4,7 +4,12 @@ const background = document.querySelector(".dropdownBackground");
 
 function handleEnter() {
   this.classList.add("trigger-enter");
-  setTimeout(() => this.classList.add("trigger-enter-active"), 150);
+  setTimeout(
+    () =>
+      this.classList.contains("trigger-enter") &&
+      this.classList.add("trigger-enter-active"),
+    150
+  );
   background.classList.add("open");
   const dropdown = this.querySelector(".dropdown");
   const dropdownCoords = dropdown.getBoundingClientRect();
